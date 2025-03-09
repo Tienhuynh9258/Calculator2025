bc634b30b0eb48d921000e39f4f0d6509d05a2c7
htn <huynhtien9258@gmail.com>
2025-03-09 08:54:45 +0700

    Updates

---
+++ precache-manifest.8a1878502cde660ddfbc78e55cfe6ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e876a9274fc33348b59a6dbc2567808a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc7243ff426fc9c1b529",
+    "url": "/static/css/main.c16154e1.chunk.css"
+  },
+  {
+    "revision": "296ecc7861554260fb20",
+    "url": "/static/js/2.990e2e36.chunk.js"
+  },
+  {
+    "revision": "dc7243ff426fc9c1b529",
+    "url": "/static/js/main.ab4a3f64.chunk.js"
+  },
+  {
+    "revision": "58345bc645d61e2a0641",
+    "url": "/static/js/runtime-main.37e0aca8.js"
+  }
+]);
